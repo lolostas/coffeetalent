61893eae36bc13765a8180ab90a3e2ed329d3285
Loic Stas <lolostas@hotmail.com>
2017-08-17 14:06:22 +0200

    CSS forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def image_path_for(user)
+    if user.facebook_picture_url.present?
+      user.facebook_picture_url
+    elsif user.photo.present?
+      cl_image_path user.photo.path
+    else
+      image_path "chien.jpg"
+    end
+  end
 end
