ea621d8f2c247785f1530931fa3328648616393e
Loic Stas <lolostas@hotmail.com>
2017-08-21 14:49:25 +0200

    Cleanup unsed stuff

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider(:linkedin, ENV['LK_CLIENT_ID'], ENV['LK_CLIENT_SECRET'], {
-#     scope: 'r_fullprofile r_emailaddress r_network',
-#     fields: ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
-#   })
-# end
