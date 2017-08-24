7647112196a2b505266949c29b43db7ac3670323
Loic Stas <lolostas@hotmail.com>
2017-08-24 18:41:54 +0200

    Mino modifs and fixes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,8 +5,8 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = current_user.mailbox.conversations.find(params[:id])
-    @conversation.receipts.each do |receipt|
-      receipt.mark_as_read if receipt.receiver == current_user
+    @conversation.receipts.inbox.each do |receipt|
+      receipt.mark_as_read
     end
     @appointment = Appointment.new
   end
